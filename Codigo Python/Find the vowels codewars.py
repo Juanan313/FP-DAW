499ece5408aab913fbc396bf871073ef549a2e44
Juanan <juanan.pujals@gmail.com>
2017-11-02 20:44:42 +0100

    Más codigo en python

--- Codigo Python/Find the vowels codewars.py
+++ Codigo Python/Find the vowels codewars.py
@@ -1,9 +1,3 @@
-# Fin the vowels code wars
-# 7th kyu
-#We want to know the index of the vowels in a given word, for example, 
-#there are two vowels in the word super (the second and fourth letters).
-
-So given a string "super", we should return a list of [2, 4].
 def vowel_indices(word):
     listWord = list(word)
     vocales ="aeiouyAEIOUY"
--- Codigo Python/Resolver equacion 2ºgrado.py
+++ Codigo Python/Resolver equacion 2ºgrado.py
@@ -2,21 +2,16 @@
 # a es el coeficiente cuadratico (distinto de 0)
 # b el coeficiente lineal 
 # c es el termino independiente
-
+def equacion2Grado (a,b,c):
 import math
-
-a = 1
-b = -5
-c = 6
 raiz = (b**2)-(4*a*c)
 if raiz>0 :
-    
     x1 = (-b + (math.sqrt(raiz)))/(2*a)
     x2 = (-b - (math.sqrt(raiz)))/(2*a)
-    print (x1)
-    print (x2)
-    
+    print "X1 es "x1, "X2 es "x2"."
 else:
-    print ('no es posible calcular raiz negativa')
+    return ('no es posible calcular raiz negativa')
+
+print equacion2Grado(1,2,3)
 
     # casos 
