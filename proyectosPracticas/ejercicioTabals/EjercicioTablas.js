d56b05befc825ea24ea8e9711ce2be6290c506cb
Unknown <juanan.pujals@gmail.com>
2018-03-19 18:29:25 +0100

    arreglos

--- proyectosPracticas/ejercicioTabals/EjercicioTablas.js
+++ proyectosPracticas/ejercicioTabals/EjercicioTablas.js
@@ -1,7 +1,31 @@
 window.onload = function() {
-
+}
     function cambiarColor1() {
-        document.getElementById("id1").style.backgroundColor = "blue";
+        var div1 = document.getElementById("id1");
+
+        if (div1.style.backgroundColor == "grey") {
+            div1.style.backgroundColor = "black";
+        } else {
+            div1.style.backgroundColor = "grey";
+        }
+
     }
 
-}
+    function cambiarColor2() {
+        var div2 = document.getElementById("id2");
+
+        if (div2.style.backgroundColor == "yellow") {
+            div2.style.backgroundColor = "green";
+        } else {
+            div2.style.backgroundColor = "yellow";
+        }
+
+    }
+
+    function crearTabla() {
+        var div1 = document.getElementById("id1");
+
+        
+    }
+
+    
