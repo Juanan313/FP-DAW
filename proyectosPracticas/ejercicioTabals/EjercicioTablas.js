1398c11b44667b01b3a10e4671c829c73c66aa1f
Juan Antonio Pujals Echevarría <jpujals@quonext-tur.local>
2018-03-20 09:51:04 +0100

    practica tablas

--- proyectosPracticas/ejercicioTabals/EjercicioTablas.js
+++ proyectosPracticas/ejercicioTabals/EjercicioTablas.js
@@ -1,7 +1,25 @@
 window.onload = function() {
+
+    document.getElementById("buttonTable1").onclick=function(){
+        crearTabla("id1");
+    };
+
+    document.getElementById("buttonTable2").onclick=function(){
+        crearTabla("id2");
+    };
+
+    document.getElementById("buttonColor1").onclick=function(){
+        cambiarColor("id1");
+    };
+
+    document.getElementById("buttonColor2").onclick=function(){
+        cambiarColor("id2");
+    };
+
 }
-    function cambiarColor1() {
-        var div1 = document.getElementById("id1");
+
+    function cambiarColor(id) {
+        var div1 = document.getElementById(id);
 
         if (div1.style.backgroundColor == "grey") {
             div1.style.backgroundColor = "black";
@@ -11,21 +29,30 @@ window.onload = function() {
 
     }
 
-    function cambiarColor2() {
+   /* function cambiarColor2() {
         var div2 = document.getElementById("id2");
 
         if (div2.style.backgroundColor == "yellow") {
             div2.style.backgroundColor = "green";
         } else {
             div2.style.backgroundColor = "yellow";
+        }screenX
+
+    } */
+
+    function crearTabla(id) {
+        var table = "<table>";
+        for ( i=0; i < 5; i++ ) {
+            table += "<tr>";
+            for ( n=1; n < 7; n++) {
+                table += " <td>"+"</td>"
+            }
+            
+            table += "</tr>";
         }
+        table += "</table>";
 
-    }
-
-    function crearTabla() {
-        var div1 = document.getElementById("id1");
-
-        
+        document.getElementById(id).innerHTML = table;
     }
 
     
