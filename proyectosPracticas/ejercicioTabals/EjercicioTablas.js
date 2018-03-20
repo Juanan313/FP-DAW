26565f2c05ea3a7edc038673ce8d36b9dc254f10
Juan Antonio Pujals Echevarría <jpujals@quonext-tur.local>
2018-03-20 13:01:58 +0100

    seleccionar y deselccionar celdas

--- proyectosPracticas/ejercicioTabals/EjercicioTablas.js
+++ proyectosPracticas/ejercicioTabals/EjercicioTablas.js
@@ -15,8 +15,52 @@ window.onload = function() {
     document.getElementById("buttonColor2").onclick=function(){
         cambiarColor("id2");
     };
+    
+    document.addEventListener("click", function(e){
+        var elemento = e.target;
 
-}
+        selectCeldas(elemento);    
+    });
+
+    document.getElementById("buttonInsertText1").onclick=function(){
+        insertarTexto("insertText1");
+    };
+
+    document.getElementById("buttonInsertText2").onclick=function(){
+        insertarTexto("insertText2");
+    };
+    
+
+    document.addEventListener("click", function(e) {
+        var elemento = e.target;
+        var escape = false;
+        
+        if (elemento.tagName == "BUTTON" || elemento.id == ("insertText1" || "insertText2")) {
+            return false;
+        }
+        while (escape == false) {
+            if (elemento == null) {
+                escape = true;
+            } else if (elemento.id == "tabla") {
+                return false;
+            }
+            else {
+                elemento = elemento.parentNode;
+            } 
+        }
+
+        deselectCeldas(elemento);
+        }
+    )};
+
+    function selectCeldas(elemento) {
+
+        if (elemento.tagName == "TD" && elemento.className != "selected" ) {
+            elemento.className = "selected";
+        } else if (elemento.tagName == "TD" && elemento.className == "selected") {
+            elemento.className = "";
+        } 
+    }
 
     function cambiarColor(id) {
         var div1 = document.getElementById(id);
@@ -29,19 +73,19 @@ window.onload = function() {
 
     }
 
-   /* function cambiarColor2() {
-        var div2 = document.getElementById("id2");
+    function insertarTexto(id) {
+        var arrayCeldas = document.getElementsByClassName("selected");
+        var textoAInsertar = document.getElementById(id).value; 
+        for (i=0; i < arrayCeldas.length; i++) {
+            /*textoAInsertar = document.createTextNode(document.getElementById(id).value);*/
+            /*arrayCeldas[i].appendChild(textoAInsertar);*/
+            arrayCeldas[i].innerHTML= textoAInsertar;
+        } 
 
-        if (div2.style.backgroundColor == "yellow") {
-            div2.style.backgroundColor = "green";
-        } else {
-            div2.style.backgroundColor = "yellow";
-        }screenX
-
-    } */
+    }
 
     function crearTabla(id) {
-        var table = "<table>";
+        var table = "<table id='tabla'>";
         for ( i=0; i < 5; i++ ) {
             table += "<tr>";
             for ( n=1; n < 7; n++) {
@@ -55,4 +99,19 @@ window.onload = function() {
         document.getElementById(id).innerHTML = table;
     }
 
+        function deselectCeldas() {
+
+        var arrayCeldas = document.getElementsByClassName("selected");
+        /*for (i=0; i < arrayCeldas.length; i++) {
+            arrayCeldas[i].className = "";
+        }*/
+
+        while (arrayCeldas.length > 0) {
+            arrayCeldas[0].className = "";
+        }
+
+        for (var celda in arrayCeldas) {
+            celda.className = "";
+        }
+    }
     
